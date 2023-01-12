dbb9301033af4ffbc600d8f01f2b1cd7be1796f4
nekonlu <appyoohoy@gmail.com>
2023-01-12 23:16:01 +0900

    Initial Commit

---
+++ SimpleToDo-MVVM/SimpleToDo_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDo_MVVMApp.swift
+//  SimpleToDo-MVVM
+//
+//  Created by Ohara Yoji on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDo_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
